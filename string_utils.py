27dad147a2a5c23f01d564317cd30ebeba9bbd2e
Guy Ness <guyness03@gmail.com>
2025-11-19 20:12:13 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -1,6 +1,21 @@
 def split_before_each_uppercases(formula):
-    pass # Replace the `pass` with your code
+    if not formula:
+        return []
+    
+    split_formula = []
+    start = 0
+    for end in range(1, len(formula)):
+        if formula[end].isupper():
+            split_formula.append(formula[start:end])
+            start = end
+    split_formula.append(formula[start:])
+    return split_formula
 
 
 def split_at_first_digit(formula):
-    pass # Replace the `pass` with your code
+    for i, char in enumerate(formula):
+        if char.isdigit():
+            prefix = formula[:i]
+            numeric_part = int(formula[i:])
+            return prefix, numeric_part
+    return formula, 1
